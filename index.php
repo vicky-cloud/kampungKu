faaf9e537ee47b353d7357d7b26e1534e943aca8
vicky-ar <vickyarisandy70@gmail.com>
2019-08-29 14:01:57 +0700

    cuman belajar bootstrap

---
+++ index.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="shortcut icon" href="assets/page.ico">
+    <title>Karang Taruna</title>
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Link Swiper's CSS -->
+  <link rel="stylesheet" type="text/css" href="assets/swipper/css/swiper.min.css">
+    <!-- Demo styles -->
+  <style>
+    .swiper-container {
+      width: 100%;
+      padding-top: 50px;
+      padding-bottom: 50px;
+    }
+    .swiper-slide {
+      background-position: center;
+      background-size: cover;
+      width: 300px;
+      height: 300px;
+
+    }
+  </style>
+  <style>
+.responsive {
+  width: 100%;
+  height: auto;
+}
+</style>
+    <link rel="stylesheet" type="text/css" href="assets/css/carousel.css">
+</head>
+<body>
+    <header>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    
+    <marquee direction="left"><a class="navbar-brand" href="#">Karang Taruna RW III oro - oro, Kel. Pacar Keling, Kec. Tambaksari, Kota Surabaya</a></marquee>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    
+  </nav>
+</header>
+
+<main role="main">
+
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+       <img class="responsive" src="image/banner.png" width="600" height="400">
+        <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"> <img class="img-responsive" alt="Responsive image"  src="image/banner.png"><rect width="100%" height="100%" fill="#777"/></svg> -->
+        <div class="container">
+          <div class="carousel-caption text-left">
+            <!-- <h1>Satu</h1>
+            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p> -->
+             <p><a class="btn btn-lg btn-success" href="#" role="button">Kunjungi</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="image/slide0.png">
+        <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg> -->
+        <div class="container">
+          <div class="carousel-caption">
+            <h1>Selamat datang, pada website karang taruna kita</h1>
+            <p>kita akan buat sesuatu...</p>
+            <p><a class="btn btn-lg btn-primary" href="#" role="button">Kunjungi</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+         <img src="image/slide1.png">
+        <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg> -->
+        <div class="container">
+          <div class="carousel-caption">
+            <!-- <h1>#PasukanLimaMenit</h1> -->
+            <h1>Bersama Bonek Plengdot</h1>
+            <p>Teman teman yang suka dengan permaianan tim bola dari kota sendiri...</p>
+            <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+
+  <!-- Marketing messaging and featurettes
+  ================================================== -->
+  <!-- Wrap the rest of the page in another container to center all the content. -->
+
+  <div class="container marketing">
+
+    <!-- Three columns of text below the carousel -->
+    <div class="row">
+       <!-- Swiper -->
+  <div class="swiper-container">
+    <div class="swiper-wrapper">
+     
+      <div class="swiper-slide" >
+        <div class="swiper-slide" style="background-image:url(assets/image/1.jpg)"></div>
+        <div><h3 align="center">blablabla ( blablabla )</h3></div>
+      </div>
+       <div class="swiper-slide" >
+        <div class="swiper-slide" style="background-image:url(assets/image/11.jpg)"></div>
+        <div><h3 align="center">blablabla ( blablabla )</h3></div>
+      </div>
+       <div class="swiper-slide" >
+        <div class="swiper-slide" style="background-image:url(assets/image/9.jpg)"></div>
+        <div><h3 align="center">blablabla ( blablabla )</h3></div>
+      </div>
+      <div class="swiper-slide" >
+        <div class="swiper-slide" style="background-image:url(assets/image/10.jpg)"></div>
+        <div><h3 align="center">blablabla ( blablabla )</h3></div>
+      </div>
+      <div class="swiper-slide" >
+        <div class="swiper-slide" style="background-image:url(assets/image/2.jpg)"></div>
+        <div><h3 align="center">blablabla ( blablabla )</h3></div>
+      </div>
+      <div class="swiper-slide" >
+        <div class="swiper-slide" style="background-image:url(assets/image/3.jpg)"></div>
+        <div><h3 align="center">blablabla ( blablabla )</h3></div>
+      </div>
+      <div class="swiper-slide" >
+        <div class="swiper-slide" style="background-image:url(assets/image/4.jpg)"></div>
+        <div><h3 align="center">blablabla ( blablabla )</h3></div>
+      </div>
+      <div class="swiper-slide" >
+        <div class="swiper-slide" style="background-image:url(assets/image/5.jpg)"></div>
+        <div><h3 align="center">blablabla ( blablabla )</h3></div>
+      </div>
+      <div class="swiper-slide" >
+        <div class="swiper-slide" style="background-image:url(assets/image/6.jpg)"></div>
+        <div><h3 align="center">blablabla ( blablabla )</h3></div>
+      </div>
+      <div class="swiper-slide" >
+        <div class="swiper-slide" style="background-image:url(assets/image/7.jpg)"></div>
+        <div><h3 align="center">blablabla ( blablabla )</h3></div>
+      </div>
+      <div class="swiper-slide" >
+        <div class="swiper-slide" style="background-image:url(assets/image/8.jpg)"></div>
+        <div><h3 align="center">blablabla ( blablabla )</h3></div>
+      </div>
+     
+     
+    </div>
+    <!-- Add Pagination -->
+    <br>
+    <div class="swiper-pagination"></div>
+  </div>
+    </div><!-- /.row -->
+
+
+    <!-- START THE FEATURETTES -->
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
+        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+      </div>
+      <div class="col-md-5">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7 order-md-2">
+        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
+        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+      </div>
+      <div class="col-md-5 order-md-1">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
+        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+      </div>
+      <div class="col-md-5">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <!-- /END THE FEATURETTES -->
+
+  </div><!-- /.container -->
+
+
+  <!-- FOOTER -->
+  <footer class="container">
+    <p class="float-right"><a href="#">Back to top</a></p>
+    <p>&copy; 2017-2019 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+  </footer>
+</main>
+<script src="assets/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- Swiper JS -->
+  <script type="text/javascript" src="assets/swipper/js/swiper.min.js"></script>
+
+  <!-- Initialize Swiper -->
+  <script>
+    var swiper = new Swiper('.swiper-container', {
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows : true,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
+  </script>
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+    <title>Karang Taruna</title>
+  </head>
+  <body>
+  		<!-- navbar -->
+  		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="#">Karang Taruna</a>
+				</div>
+				<ul class="nav justify-content-end">
+				  <li class="nav-item">
+				    <a class="nav-link active" href="#">Active</a>
+				  </li>
+				  <li class="nav-item">
+				    <a class="nav-link" href="#">Link</a>
+				  </li>
+				  <li class="nav-item">
+				    <a class="nav-link" href="#">Link</a>
+				  </li>
+				  <li class="nav-item">
+				    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+				  </li>
+				</ul>
+			</div>
+		</nav>
+  		<!-- navbar -->
+    <h1>Hello, world!</h1>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="assets/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
